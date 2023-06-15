4b667d99f1abb3bc784568b1a5358ba8224502d8
VivekDreamer <vivekmakv@gmail.com>
2023-06-15 23:32:18 +0530

    added comment

--- src/main/java/org/vivek/dp/knapsack/CountNumOfSubsetWithGivenDiff.java
+++ src/main/java/org/vivek/dp/knapsack/CountNumOfSubsetWithGivenDiff.java
@@ -37,6 +37,11 @@ public class CountNumOfSubsetWithGivenDiff {
         if((sum+d)%2 != 0) return 0;
         int sum1 = (sum+d)/2;
         // reduced : find counts of subset with sum equal to t
+        // 1)  We are initialising first column to 1, assuming there is only 1 way to make subset sum equal to 0,  i.e. null subset, BUT this fails if we have 0's as elements of array. If we have a single 0 present in the array, then the subsets will be '{}, {0}' whose sum will be 0. Hence, there can be more than 1 way to make sum==0.
+        // FIX: Don't initialise the first col to be 1. Everything will be initialized to 0 except the first cell in the table i.e. dp[0][0]=1. AND j will start from 0 instead of 1.
+        // 2)  In the derived formula, target = (diff+totalSum) / 2, NOTICE that (diff+totalSum) must be even for target to be a whole number, else it's not possible to find a decimal target in subset sum. Hence, we also need to take care of the test cases where it's not possible to partition the array.
+        // FIX: Check, if it's odd,  there is no way --> if((diff+totalSum)%2 != 0) return 0;
+
         int[][] dp = new int[n+1][sum1+1];
         dp[0][0] = 1;
         for(int i = 1; i < n+1; i++){
