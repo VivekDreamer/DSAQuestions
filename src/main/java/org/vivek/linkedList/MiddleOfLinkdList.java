733214af221c497d9dc2ed26c42b1f2bbd008053
VivekDreamer <vivekmakv@gmail.com>
2023-07-06 23:22:33 +0530

    3rd question -> find the middle of the linked list

---
+++ src/main/java/org/vivek/linkedList/MiddleOfLinkdList.java
@@ -0,0 +1,16 @@
+package org.vivek.linkedList;
+
+public class MiddleOfLinkdList {
+    public static void main(String[] args) {
+        
+    }
+    public ListNode middleNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+}
