7f412696ca1cfb0a9dcd58a9b7dbc82943fa10fc
VivekDreamer <vivekmakv@gmail.com>
2023-07-11 23:32:05 +0530

    modified lru cache question definition

--- src/main/java/org/vivek/linkedList/LRUCache.java
+++ src/main/java/org/vivek/linkedList/LRUCache.java
@@ -3,6 +3,14 @@ package org.vivek.linkedList;
 import java.util.HashMap;
 import java.util.Map;
 
+//Design a data structure that follows the constraints of a Least Recently Used (LRU) cache.
+
+// Implement the LRUCache class:
+
+// LRUCache(int capacity) Initialize the LRU cache with positive size capacity.
+// int get(int key) Return the value of the key if the key exists, otherwise return -1.
+// void put(int key, int value) Update the value of the key if the key exists. Otherwise, add the key-value pair to the cache. If the number of keys exceeds the capacity from this operation, evict the least recently used key.
+
 public class LRUCache {
     static class Node{
         Node prev, next;
