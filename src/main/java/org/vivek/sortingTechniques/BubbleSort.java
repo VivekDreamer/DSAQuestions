c811c00a267d55ab8d985c6b01865801da7c6a3a
VivekDreamer <vivekmakv@gmail.com>
2023-07-13 17:29:49 +0530

    some changes in the bubble sort

--- src/main/java/org/vivek/sortingTechniques/BubbleSort.java
+++ src/main/java/org/vivek/sortingTechniques/BubbleSort.java
@@ -2,8 +2,8 @@ package org.vivek.sortingTechniques;
 
 import java.util.Arrays;
 
+//bubble sort is stable algorithm as it never changes order of equal elements and it is in place
 public class BubbleSort {
-    
     public static void main(String[] args) {
         int[] arr = {7,8,3,1,2};
         System.out.println("Before sorting : "+Arrays.toString(arr));
@@ -14,13 +14,30 @@ public class BubbleSort {
         for(int i = 0; i < arr.length-1; i++){
             for(int j = 0; j < arr.length-i-1; j++){
                 //swap 
-                //tske the heaviest bubble at the end of array by comparing and then, sorting
+                //take the heaviest bubble at the end of array by comparing and then, sorting
                 if(arr[j] > arr[j+1]){
                     int temp = arr[j];
                     arr[j] = arr[j+1];
                     arr[j+1] = temp;
                 }
             }
+        }
+    }
+    //if array becomes sorted in the mid, then, it will not do further proocessing
+    public static void bubbleSortOptimized(int[] arr){
+        for(int i = 0; i < arr.length-1; i++){
+            boolean swapped = false;
+            for(int j = 0; j < arr.length-i-1; j++){
+                //swap 
+                //take the heaviest bubble at the end of array by comparing and then, sorting
+                if(arr[j] > arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                    swapped = true;
+                }
+            }
+            if(swapped == false)break;
         }
     }
 }
