86a01823d4d3686768a70475ad5a2374dffa1d35
VivekDreamer <vivekmakv@gmail.com>
2023-08-12 22:50:46 +0530

    adding trees question-> basic tree node structure

---
+++ src/main/java/org/vivek/allCategoriesQustions/trees/Node.java
@@ -0,0 +1,11 @@
+package org.vivek.allCategoriesQustions.trees;
+
+public class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        this.left = this.right = null;
+    }
+}
