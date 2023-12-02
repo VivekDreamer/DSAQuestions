2fb33212cf796d059b6a531f9c466e4b8395f4d1
VivekDreamer <vivekmakv@gmail.com>
2023-12-02 18:16:27 +0530

    Leetcode string question: Find the words that can be formed by the characters given.

---
+++ src/main/java/org/vivek/allCategoriesQustions/strings/Leetcode_FindWordsThatCanBeFormedByCharacters.java
@@ -0,0 +1,28 @@
+package org.vivek.allCategoriesQustions.strings;
+// https://leetcode.com/problems/find-words-that-can-be-formed-by-characters/
+public class Leetcode_FindWordsThatCanBeFormedByCharacters {
+    public int countCharacters(String[] words, String chars) {
+        int[] charCount = new int[26];
+        for (char ch : chars.toCharArray()) {
+            charCount[ch - 'a']++;
+        }
+        int result = 0;
+        for (String word : words) {
+            int[] wordCount = new int[26];
+            for (char ch : word.toCharArray()) {
+                wordCount[ch - 'a']++;
+            }
+            boolean ok = true;
+            for (int i = 0; i < 26; i++) {
+                if (wordCount[i] > charCount[i]) {
+                    ok = false;
+                    break;
+                }
+            }
+            if (ok) {
+                result += word.length();
+            }
+        }
+        return result;
+    }
+}
