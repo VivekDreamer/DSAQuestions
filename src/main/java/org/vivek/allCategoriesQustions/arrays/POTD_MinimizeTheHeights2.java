e8c784e8dd33cd6ee2f3c8287534c5543e1758d7
VivekDreamer <vivekmakv@gmail.com>
2023-12-05 20:44:55 +0530

    POTD - Minimize the heights II

---
+++ src/main/java/org/vivek/allCategoriesQustions/arrays/POTD_MinimizeTheHeights2.java
@@ -0,0 +1,21 @@
+package org.vivek.allCategoriesQustions.arrays;
+
+import java.util.Arrays;
+
+public class POTD_MinimizeTheHeights2 {
+    int getMinDiff(int[] arr, int n, int k) {
+        if(n == 1) return 0;
+        Arrays.sort(arr);
+        int max = 0, min = 0;
+        int diff = arr[n-1]-arr[0];
+        
+        for(int i = 1; i < n; i++){
+            if(arr[i]-k < 0) continue;
+            max = Math.max(arr[i-1]+k , arr[n-1]-k);
+            min = Math.min(arr[0]+k , arr[i]-k);
+            diff = Math.min(diff,max-min);
+        }
+        
+        return diff;
+    }
+}
