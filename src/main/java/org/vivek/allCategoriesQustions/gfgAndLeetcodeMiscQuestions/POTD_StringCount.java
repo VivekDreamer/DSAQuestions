905576bcf27b248cb2be6554305861e00ce944a5
VivekDreamer <vivekmakv@gmail.com>
2023-12-18 20:09:18 +0530

    POTD question updated : String count

--- src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_StringCount.java
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_StringCount.java
@@ -1,8 +1,10 @@
+package org.vivek.allCategoriesQustions.gfgAndLeetcodeMiscQuestions;
+
+// https://www.geeksforgeeks.org/problems/count-of-strings-that-can-be-formed-using-a-b-and-c-under-given-constraints1135/1
 class POTD_StringCount 
 { 
     static long countStr(long n)
 	{
-	    // code here
-return 1+ 2*n + n*(n-1)*(n+1)/2;
+		return 1+ 2*n + n*(n-1)*(n+1)/2;
 	}
 } 
