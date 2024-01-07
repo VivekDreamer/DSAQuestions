1d90cc3d5aeeb3bdf7d8c352c7a24f3db44ce87f
VivekDreamer <vivekmakv@gmail.com>
2024-01-07 13:06:35 +0530

    GFG POTD  : Split Array Largest Sum

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_SplitArray.java
@@ -0,0 +1,36 @@
+package org.vivek.allCategoriesQustions.gfgAndLeetcodeMiscQuestions;
+
+import java.util.Arrays;
+
+//https://www.geeksforgeeks.org/problems/split-array-largest-sum--141634/1
+public class POTD_SplitArray {
+    static int splitArray(int[]arr,int N,int K){
+        long low= Arrays.stream(arr).max().getAsInt();
+        long high= Arrays.stream(arr).sum();
+        while(low<high){
+            long mid = low +(high - low) /2;
+            int count= getCount(arr, N, mid);
+            
+            if(count > K){
+                low =mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        return (int)low;
+    }
+    static int getCount(int arr[], int N, long maxSum){
+        int count =1;
+         long currentSum = 0;
+        
+        for (int num : arr) {
+            currentSum += num;
+            if (currentSum > maxSum) {
+                count++;
+                currentSum = num;
+            }
+        }
+        
+        return count;
+    }
+}
