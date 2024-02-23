ed10dcd177aebd761e05c4e7cbc2fec41abb1790
VivekDreamer <vivekmakv@gmail.com>
2024-02-23 20:09:00 +0530

    Leetcode : Flood Fill

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/Leetcode_FloodFill.java
@@ -0,0 +1,19 @@
+package org.vivek.allCategoriesQustions.gfgAndLeetcodeMiscQuestions;
+public class Leetcode_FloodFill {
+    public int[][] floodFill(int[][] image, int sr, int sc, int newColor) {
+        if(image[sr][sc] == newColor) return image;
+        fillColor( image, image[sr][sc], sr, sc, newColor);
+        return image;
+       }
+        public void fillColor(int[][] image, int oldColor, int sr, int sc, int newColor) {
+           
+            if(sc<0 || sr<0 || sr >= image.length || sc >= image[0].length || image[sr][sc] !=          oldColor) return;
+            
+            image[sr][sc] = newColor;
+            fillColor( image, oldColor, sr-1, sc, newColor);
+            fillColor( image, oldColor, sr+1, sc, newColor);
+            fillColor( image, oldColor, sr, sc-1, newColor);
+            fillColor( image, oldColor, sr, sc+1, newColor);
+            
+        }
+}
