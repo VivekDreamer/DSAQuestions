052e544437f6ac778bc97d71515699d192e578a8
VivekDreamer <vivekmakv@gmail.com>
2024-03-13 18:35:43 +0530

    GFG POTD : Print matrix in diagonal pattern

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_PrintMatrixInDiagonalPattern.java
@@ -0,0 +1,44 @@
+package org.vivek.allCategoriesQustions.gfgAndLeetcodeMiscQuestions;
+
+// https://www.geeksforgeeks.org/problems/print-matrix-in-diagonal-pattern/1
+public class POTD_PrintMatrixInDiagonalPattern {
+    public int[] matrixDiagonally(int[][] mat) {
+        int n = mat.length;
+        int ans[] = new int[n*n];
+        int k=0, i=0, j=0;
+        int upper=1;
+        while(k< n*n){
+            if(upper==1){
+                // Traverse upper diagonal
+                while(i>=0 && j<n){
+                    ans[k++] = mat[i][j];
+                    if(j==n-1){ // Switch to lower diagonal starting from next row
+                        i++; break;
+                    }
+                    else if(i==0){ // Switch to lower diagonal starting from next column
+                        j++; break;
+                    } 
+                    // Move top right
+                    i--; j++;
+                }
+                upper=0;
+            }
+            else{
+                // Traverse lower diagonal
+                while(i<n && j>=0){
+                    ans[k++] = mat[i][j];
+                    if(i==n-1){ // Switch to upper diagonal starting from next column
+                        j++; break;
+                    }
+                    else if(j==0 ){  // Switch to upper diagonal starting from next row
+                        i++; break;
+                    }
+                    // Move bottom left
+                    i++; j--;
+                }
+                upper=1;
+            }   
+        }
+        return ans;
+    }
+}
