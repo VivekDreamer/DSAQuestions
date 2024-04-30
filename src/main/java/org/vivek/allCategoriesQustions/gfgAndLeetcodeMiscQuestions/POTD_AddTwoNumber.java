8f9f3a1cb435151b299ee640363aac9cd98c77e3
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-04-30 05:58:11 +0530

    GFG POTD : Add two Numbers

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_AddTwoNumber.java
@@ -0,0 +1,48 @@
+// https://www.geeksforgeeks.org/problems/add-two-numbers-represented-by-linked-lists/1
+class Node {
+    int data;
+    Node next;
+
+    Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+*/
+// 5->4
+// 5->4->3
+public class POTD_AddTwoNumber{
+    //Function to add two numbers represented by linked list.
+    static Node addTwoLists(Node first, Node second){
+        first = reverseList(first);
+        second = reverseList(second);
+        int carry = 0;
+        Node dummy = new Node(-1);
+        Node temp = dummy;
+        while(first != null || second != null || carry == 1){
+            int a = (first != null) ? first.data : 0;
+            int b = (second != null) ? second.data : 0;
+            int sum = a + b + carry;
+            temp.next = new Node(sum % 10);
+            temp = temp.next;
+            carry = sum / 10;
+            if(first != null) first = first.next;
+            if(second != null) second = second.next;
+        }
+        Node ans = reverseList(dummy.next);
+        while(ans != null && ans.data == 0) ans = ans.next;
+        return (ans==null) ? new Node(0) : ans;
+    }
+    static Node reverseList(Node head){
+        if(head == null || head.next == null) return head;
+        Node prev = null, curr = head, next = null;
+        while(curr != null){
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+}
