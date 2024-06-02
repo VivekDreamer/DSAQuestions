8f77c728405e9baa34d2c7e2ad212bcc3f7d0c57
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-06-02 17:15:52 +0530

    GFG POTD : Construct list using given q XOR queries

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_ConstructListUsingGivenQXORQueries.java
@@ -0,0 +1,15 @@
+// https://www.geeksforgeeks.org/problems/construct-list-using-given-q-xor-queries/1
+class POTD_ConstructListUsingGivenQXORQueries {
+    public static ArrayList<Integer> constructList(int q, int[][] queries) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        int xor = 0;
+        for(int i = q-1; i >= 0; i--){
+            if(queries[i][0] == 0) ans.add(queries[i][1]^xor);
+            else xor^=queries[i][1];
+        }
+        ans.add(xor);
+        // Collections.reverse(ans);
+        Collections.sort(ans);
+        return ans;
+    }
+}
