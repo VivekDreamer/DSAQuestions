077a12946021a9bce05e2edeebb8025f70c4f7bb
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-07-08 19:43:47 +0530

    GFG POTD : Search in Rotated Sorted Array

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_SearchInRotatedSortedArray.java
@@ -0,0 +1,33 @@
+// https://www.geeksforgeeks.org/problems/search-in-a-rotated-array4618/1
+class POTD_SearchInRotatedSortedArray {
+    int search(int[] arr, int key) {
+        int pivotIdx = getPivot(arr,arr.length);
+        // System.out.println(pivotIdx);
+        int result = -1;
+        if(key >= arr[pivotIdx] && key <= arr[arr.length-1]) result = binarySearch(arr,pivotIdx,arr.length-1,key);
+        else result = binarySearch(arr,0,pivotIdx-1,key);
+        return result;
+    }
+    int getPivot(int[] arr, int n){
+        if(arr[0] < arr[n-1] || n == 1) return 0;
+        int start = 0;
+        int end = n-1;
+        int mid = start + (end - start)/2;
+        while(start < end){
+            if(arr[mid] >= arr[0]) start = mid+1;
+            else end = mid;
+            mid = start + (end-start)/2;
+        }
+        return start;
+    }
+    int binarySearch(int[] arr, int start, int end, int key){
+        int mid = start+(end-start)/2;
+        while(start<=end){
+            if(arr[mid] == key) return mid;
+            if(arr[mid] < key) start = mid + 1;
+            else end = mid - 1;
+            mid = start + (end - start)/2;
+        }
+        return -1;
+    }
+}
