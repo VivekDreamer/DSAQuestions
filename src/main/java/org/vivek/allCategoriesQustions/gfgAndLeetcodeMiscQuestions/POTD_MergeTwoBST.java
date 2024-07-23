be4a044dc23a7f7d84510fa9fd453b9c7c1b91aa
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-07-23 20:35:55 +0530

    GFG POTD : Merge two BST 's

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_MergeTwoBST.java
@@ -0,0 +1,57 @@
+// https://www.geeksforgeeks.org/problems/merge-two-bst-s/1
+class Node  
+{ 
+    int data; 
+    Node left, right; 
+   
+    public Node(int d)  
+    { 
+        data = d; 
+        left = right = null; 
+    } 
+}
+  
+public class POTD_MergeTwoBST
+{
+    //Function to return a list of integers denoting the node 
+    //values of both the BST in a sorted order.
+    public List<Integer> merge(Node root1,Node root2)
+    {
+        // Write your code here
+        List<Integer> FirstBst = new ArrayList<>();
+        List<Integer> SecondBst = new ArrayList<>();
+        List<Integer> MergedBst = new ArrayList<>();
+        inorderBst(root1,FirstBst);
+        inorderBst(root2,SecondBst);
+        //Collections.sort(list);
+        mergeTwoSortedList(FirstBst,SecondBst,MergedBst);
+        return MergedBst;
+    }
+    void inorderBst(Node root, List<Integer> list){
+        if(root == null)return;
+        inorderBst(root.left,list);
+        list.add(root.data);
+        inorderBst(root.right,list);
+    }
+    void mergeTwoSortedList(List<Integer> FirstBst, List<Integer> SecondBst, List<Integer> MergedBst){
+        int i=0,j=0;
+        while(i < FirstBst.size() && j < SecondBst.size()){
+            if(FirstBst.get(i)<SecondBst.get(j)){
+                MergedBst.add(FirstBst.get(i));
+                i++;
+            }
+            else{
+                MergedBst.add(SecondBst.get(j));
+                j++;
+            }
+        }
+        while(i < FirstBst.size()){
+            MergedBst.add(FirstBst.get(i));
+            i++;
+        }
+        while(j < SecondBst.size()){
+            MergedBst.add(SecondBst.get(j));
+            j++;
+        }
+    }
+}
