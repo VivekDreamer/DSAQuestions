499813030492666c578db852f6f551af84f13139
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-07-26 20:43:33 +0530

    GFG POTD : K Pangrams

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_KPangrams.java
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/k-pangrams0909/1
+class POTD_KPangrams {
+    boolean kPangram(String str, int k) {
+        int alphaCount = 0;
+        int duplicatesCount = 0;
+        HashSet<Character> alpha = new HashSet<>();
+        
+        for (int i = 0; i < str.length(); i++) {
+            if (alpha.contains(str.charAt(i))){
+                duplicatesCount += 1;
+            } else if (str.charAt(i) != ' '){
+                alpha.add(str.charAt(i));
+            }
+        }
+    
+        
+        if (alpha.size() == 26 || ((26 - alpha.size() <= duplicatesCount) && (26 - alpha.size()) <= k)){
+            return true;
+        } else {
+            return false;
+        }
+        
+    }
+}
