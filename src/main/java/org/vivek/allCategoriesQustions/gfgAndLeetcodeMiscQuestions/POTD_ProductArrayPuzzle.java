5e0b938c80f06bc1d6cd9368b7b28816ab1b49ed
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-08-17 20:21:21 +0530

    GFG POTD : Product array puzzle

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_ProductArrayPuzzle.java
@@ -0,0 +1,25 @@
+// https://www.geeksforgeeks.org/problems/product-array-puzzle4525/1
+class POTD_ProductArrayPuzzle {
+    public static long[] productExceptSelf(int nums[]) {
+        long temp = 1;
+        long c = 0; 
+        int index = -1;
+        for(int i = 0 ;i<nums.length;i++){
+            if(nums[i]!=0) temp = temp*nums[i];
+            else{
+                c++;
+                index = i;
+            }
+        }
+        long ans [] = new long[nums.length];
+        if(c>1) return ans;
+        if(c==1){
+            ans[index] = temp;
+            return ans;
+        }
+        for(int i  =0 ; i<nums.length;i++){
+            ans[i] = temp/nums[i];
+        }
+        return ans;
+    }
+}
