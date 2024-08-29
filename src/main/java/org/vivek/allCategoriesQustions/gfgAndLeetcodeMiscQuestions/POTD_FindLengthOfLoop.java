570f952449d4804cdcbd6b79529ba8807f0bac8b
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-08-29 19:28:44 +0530

    GFG POTD : Find length of loop

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_FindLengthOfLoop.java
@@ -0,0 +1,24 @@
+// https://www.geeksforgeeks.org/problems/find-length-of-loop/1
+class POTD_FindLengthOfLoop {
+    // Function to find the length of a loop in the linked list.
+    public int countNodesinLoop(Node head) {
+        // Add your code here.
+        if (head == null || head.next == null) return 0;
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) { 
+                int loopSize = 1;
+                Node current = slow;
+                while (current.next != slow) {
+                    current = current.next;
+                    loopSize++;
+                }
+                return loopSize;
+            }
+        }
+        return 0; 
+    }
+}
