892a742a753a211a7af176d10256a3ed5dbbd2d3
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-09-04 20:32:18 +0530

    GFG POTD : Count ways to N'th Stair(Order does not matter)

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_CountWaysToNThStair.java
@@ -0,0 +1,8 @@
+// https://www.geeksforgeeks.org/problems/count-ways-to-nth-stairorder-does-not-matter5639/1
+class POTD_CountWaysToNThStair {
+    public int nthStair(int n) {
+        int ans = 0;
+        ans = n/2+1;
+        return ans;
+    }
+}
