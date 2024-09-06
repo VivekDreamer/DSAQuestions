f029b9f8988626f40ffeab0bf8615951a00c3a27
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-09-06 19:53:29 +0530

    GFG POTD : Kadane

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_Kadane.java
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/kadanes-algorithm-1587115620/1
+class POTD_Kadane{
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long maxSubarraySum(int arr[]){
+        int n = arr.length;
+        long sum = 0;
+        long res = Integer.MIN_VALUE;
+        for(int i = 0; i < n; i++){
+            sum += arr[i];
+            res = Math.max(res,sum);
+            if(sum <= 0) sum = 0;
+        }
+        return res;
+    }
+    
+}
