bfd6a3fd9e8bf2e75e7ff7995f2ffbc2fa0ff1aa
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-11-23 18:18:12 +0530

    GFG POTD : Minimize the Heights I

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_MinimizeTheHeightsI.java
@@ -0,0 +1,14 @@
+// https://www.geeksforgeeks.org/problems/minimize-the-heights-i/1
+public class POTD_MinimizeTheHeightsI {
+    public int getMinDiff(int k, int[] arr) {
+        int n=arr.length;
+        Arrays.sort(arr);
+        int ans=arr[n-1]-arr[0];
+        for(int i=1;i<n;i++){
+                int max=Math.max(arr[i-1]+k,arr[n-1]-k);
+                int min=Math.min(arr[0]+k,arr[i]-k);
+                ans=Math.min(ans,max-min);
+        }
+        return ans;
+    }
+}
