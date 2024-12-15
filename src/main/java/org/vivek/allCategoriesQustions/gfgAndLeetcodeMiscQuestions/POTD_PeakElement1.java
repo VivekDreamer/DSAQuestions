cf63052091422e6089b0161db5499a5e05fa0ea3
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2024-12-15 21:30:01 +0530

    GFG POTD : Peak Elem

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_PeakElement1.java
@@ -0,0 +1,20 @@
+// https://www.geeksforgeeks.org/problems/peak-element/1
+public class POTD_PeakElement1 {
+    public int peakElement(int[] arr) {
+       int n = arr.length;
+       if(n == 1) return 0;
+       if(arr[0] > arr[1]) return 0;
+       if(arr[n-1] > arr[n-2]){
+          return n-1;  
+       } 
+       int low = 1 ;
+       int high = n-2;
+       while(low <= high){
+           int mid = low + (high - low)/2;
+           if(arr[mid] >= arr[mid - 1] && arr[mid] >= arr[mid+1]) return mid;
+           else if(arr[mid] > arr[mid+1]) high = mid - 1;
+           else low = mid + 1;
+       }
+       return 0;
+    }
+}
