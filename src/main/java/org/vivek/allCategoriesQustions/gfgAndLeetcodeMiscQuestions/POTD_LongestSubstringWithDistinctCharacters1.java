155fb6aae1d849e1b093185ba2d27e62d884577a
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-01-11 20:29:06 +0530

    GFG POTD : Longest substring with distinct characters

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_LongestSubstringWithDistinctCharacters1.java
@@ -0,0 +1,18 @@
+// https://www.geeksforgeeks.org/problems/longest-distinct-characters-in-string5848/1
+class POTD_LongestSubstringWithDistinctCharacters1{
+    public int longestUniqueSubstr(String s) {
+        // code here
+        int[] arr=new int[26];
+        int l=0;
+        int maxlen=0;
+        for(int r=0;r<s.length();r++){
+            int charIndex=s.charAt(r)-'a';
+            if(arr[charIndex]!=0){
+                l=Math.max(l,arr[charIndex]);
+            }
+            arr[charIndex]=r+1;
+            maxlen=Math.max(maxlen,r-l+1);
+        }
+        return maxlen;
+    }
+}
