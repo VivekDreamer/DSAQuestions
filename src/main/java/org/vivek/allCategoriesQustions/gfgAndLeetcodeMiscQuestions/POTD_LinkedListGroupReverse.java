8a9cc30174c95325a569d794e69a2a3da9c799f3
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-01-21 19:01:47 +0530

    GFG POTD : Linked List Group Reverse

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_LinkedListGroupReverse.java
@@ -0,0 +1,45 @@
+// https://www.geeksforgeeks.org/problems/reverse-a-linked-list-in-groups-of-given-size/1
+class Node
+{
+    int data;
+    Node next;
+    Node(int key)
+    {
+        data = key;
+        next = null;
+    }
+}
+public class POTD_LinkedListGroupReverse {
+    public static Node reverseKGroup(Node head, int k) {
+        // code here
+        if(head==null) return head;
+        if(len(head)<k) return rev(head);
+        Node curr=head,next=null,prev=null;
+        for(int i=0;i<k;i++){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+        }
+        head.next=reverseKGroup(curr,k);
+        return prev;
+    }
+    private static int len(Node head){
+        int n=0;
+        while(head!=null){
+            n++;
+            head=head.next;
+        }
+        return n;
+    }
+    private static Node rev(Node head){
+        Node curr=head,next=null,prev=null;
+        while(curr!=null){
+           next=curr.next;
+           curr.next=prev;
+           prev=curr;
+           curr=next;
+        }
+        return prev;
+    }
+}
