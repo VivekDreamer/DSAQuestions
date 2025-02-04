af74a4f1eb6e4a86dbefc6b5c447c326297fd17f
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-02-04 20:34:21 +0530

    Leetcode : remove duplicates from sorted list

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/Leetcode_RemoveDuplicatesFromSortedList.java
@@ -0,0 +1,23 @@
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+public class Leetcode_RemoveDuplicatesFromSortedList {
+    public ListNode deleteDuplicates(ListNode head) {
+        if(head==null || head.next == null)
+            return head;
+        ListNode current = head;
+        while(current != null && current.next != null){
+            if(current.val == current.next.val){
+                current.next = current.next.next;
+            }
+            else{
+                current = current.next;
+            }
+        }
+        return head;
+    }
+}
