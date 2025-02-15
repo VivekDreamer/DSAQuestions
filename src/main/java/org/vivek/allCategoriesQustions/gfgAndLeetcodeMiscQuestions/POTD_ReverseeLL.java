f178bad17826db8edee376ff345e6739df88ec51
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-02-15 20:11:50 +0530

    Leetcode : Reverse LL

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_ReverseeLL.java
@@ -0,0 +1,25 @@
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
+ 
+public class POTD_ReverseeLL {
+    public ListNode reverseList(ListNode head) {
+        if( head == null || head.next == null)
+            return head;
+        ListNode current = head;
+        ListNode previous = null;
+        ListNode forward = null;
+        
+        while(current != null){
+            forward = current.next;
+            current.next = previous;
+            previous = current;
+            current = forward;
+        }
+        return previous;
+    }
+}
