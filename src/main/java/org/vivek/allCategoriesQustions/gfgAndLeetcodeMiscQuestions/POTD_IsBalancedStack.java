68970c67476b05c7fc25b81329b531591e109c0d
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-02-21 22:19:14 +0530

    GFG POTD : Is Balanced Stack

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_IsBalancedStack.java
@@ -0,0 +1,12 @@
+class POTD_IsBalancedStack {
+    static boolean isBalanced(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0; i < s.length(); i++){
+            if(s.charAt(i) == '[') stack.push(']');
+            else if(s.charAt(i) == '{') stack.push('}');
+            else if(s.charAt(i) == '(') stack.push(')');
+            else if(stack.isEmpty() || stack.pop() != s.charAt(i)) return false;
+        }
+        return stack.isEmpty();
+    }
+}
