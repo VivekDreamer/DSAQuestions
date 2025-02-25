54dc6a036ed020744e6033d2f40c7d4de4dc32b6
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-02-25 22:31:01 +0530

    GFG POTD : Get Max Area

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_GetMaxArea.java
@@ -0,0 +1,18 @@
+class POTD_GetMaxArea {
+    public static int getMaxArea(int arr[]) {
+        int n = arr.length;
+        Stack<Integer> stack = new Stack<>();
+        int maxArea = 0;
+
+        for (int i = 0; i <= n; i++) {
+            int currentHeight = (i == n) ? 0 : arr[i];
+            while (!stack.isEmpty() && currentHeight < arr[stack.peek()]) {
+                int height = arr[stack.pop()];
+                int width = stack.isEmpty() ? i : i - stack.peek() - 1;
+                maxArea = Math.max(maxArea, height * width);
+            }
+            stack.push(i);
+        }
+        return maxArea;
+    }
+}
