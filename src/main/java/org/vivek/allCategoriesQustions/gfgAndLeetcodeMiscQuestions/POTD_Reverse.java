540b15ced87bbf2dd3bf11b1520c9c3c90a92f82
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-03-25 20:53:56 +0530

    GFG POTD : Reverse

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/POTD_Reverse.java
@@ -0,0 +1,26 @@
+class ListNode {
+     int val;
+     ListNode next;
+     ListNode() {}
+     ListNode(int val) { this.val = val; }
+     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ }
+public class POTD_Reverse {
+    public ListNode reverseBetween(ListNode head, int left, int right) {
+        if(head == null || head.next == null) return head;
+        ListNode dummy = new ListNode(-1);
+        dummy.next = head;
+        ListNode prev = dummy;
+        for(int i = 1; i < left; i++){
+            prev = prev.next;
+        }
+        ListNode curr = prev.next;
+        for(int i = 1; i <= (right-left); i++){
+            ListNode temp = prev.next;
+            prev.next = curr.next;
+            curr.next = curr.next.next;
+            prev.next.next = temp;
+        }
+        return dummy.next;
+    }
+}
