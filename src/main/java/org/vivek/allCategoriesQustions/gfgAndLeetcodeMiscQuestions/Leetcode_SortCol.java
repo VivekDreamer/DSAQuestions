10488e5352bbc1116494b41b441ee4b18b150f8c
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-04-01 18:53:21 +0530

    Leetcode : SortColor

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/Leetcode_SortCol.java
@@ -0,0 +1,15 @@
+class Leetcode_SortCol {
+    public void sortColors(int[] nums) {
+        for(int i = 0; i < nums.length-1; i++){
+            int temp = 0;
+            int minIndex = i;
+            for(int j = i+1; j < nums.length; j++){
+                if(nums[j] < nums[minIndex])
+                    minIndex = j;
+            }
+            temp = nums[minIndex];
+            nums[minIndex] = nums[i];
+            nums[i] = temp;
+        }
+    }
+}
