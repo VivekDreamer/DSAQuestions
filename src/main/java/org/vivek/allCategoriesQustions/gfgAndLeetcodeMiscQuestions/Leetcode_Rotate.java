b8d6498c75ae010e10d0128f826f044c921cc538
Vivek Dhyani <90622158+VivekDreamer@users.noreply.github.com>
2025-04-06 19:33:18 +0530

    Create Leetcode_Rotate.java

---
+++ src/main/java/org/vivek/allCategoriesQustions/gfgAndLeetcodeMiscQuestions/Leetcode_Rotate.java
@@ -0,0 +1,27 @@
+class Leetcode_Rotate {
+      public void rotate(int[][] matrix) {
+        for(int i = 0; i < matrix.length; i++){
+            for(int j = i+1; j < matrix[0].length; j++){
+            	if(i!=j) {
+            		int tmp = matrix[i][j];
+            		matrix[i][j] = matrix[j][i];
+            		matrix[j][i] = tmp;
+            	}	   
+            }
+        }
+        rowSwap(matrix);
+    }
+    public void rowSwap(int[][] matrix){
+    	for(int i = 0; i < matrix.length; i++) {
+    		int start = 0;
+        	int end = matrix[0].length-1;
+        	while(start < end) {
+        		int tmp = matrix[i][start];
+        		matrix[i][start] = matrix[i][end];
+        		matrix[i][end] = tmp;
+        		start++;
+        		end--;
+        	}
+        }
+    }
+}
